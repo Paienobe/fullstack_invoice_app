5e7ed7d8d3789625f66defae507b95688ea01b66
Progress Aienobe <paienobe@gmail.com>
2024-06-15 15:04:57 +0100

    updated_invoice_id_generator_function

--- client/invoice_app/src/components/HomePageComponents/InvoiceBar/InvoiceBar.tsx
+++ client/invoice_app/src/components/HomePageComponents/InvoiceBar/InvoiceBar.tsx
@@ -3,7 +3,7 @@ import { InvoiceBarProps } from "./types";
 
 const InvoiceBar = ({ invoice }: InvoiceBarProps) => {
   return (
-    <div className="h-[5.125rem] bg-white rounded-lg border-[1px] border-transparent hover:border-purple transition-colors duration-300 cursor-pointer py-4 px-6 grid grid-cols-5 items-center font-light gap-8">
+    <div className="h-[5.125rem] bg-white rounded-lg border-[1px] border-transparent hover:border-purple transition-colors duration-300 cursor-pointer py-4 px-6 grid grid-cols-custom items-center font-light gap-8">
       <div>
         <p className="font-semibold text-text_dark">
           <span className="font-normal text-primary_text_color">#</span>
@@ -16,7 +16,7 @@ const InvoiceBar = ({ invoice }: InvoiceBarProps) => {
       <div>
         <p className="truncate">{invoice.client_name}</p>
       </div>
-      <div className="font-bold text-text_dark text-[1.125rem]">
+      <div className="font-bold text-text_dark text-right text-[1.125rem]">
         <p>£{invoice.total}</p>
       </div>
       <div className="flex items-center justify-center">
