a1431a485400601735da3349bc5bbce018faa70d
Progress Aienobe <paienobe@gmail.com>
2024-06-15 19:22:45 +0100

    display_invoices

--- client/invoice_app/src/components/HomePageComponents/InvoiceBar/InvoiceBar.tsx
+++ client/invoice_app/src/components/HomePageComponents/InvoiceBar/InvoiceBar.tsx
@@ -1,9 +1,10 @@
 import StatusTile from "../../UI/StatusTile/StatusTile";
 import { InvoiceBarProps } from "./types";
+import rightArrow from "../../../assets/icon-arrow-right.svg";
 
 const InvoiceBar = ({ invoice }: InvoiceBarProps) => {
   return (
-    <div className="h-[5.125rem] bg-white rounded-lg border-[1px] border-transparent hover:border-purple transition-colors duration-300 cursor-pointer py-4 px-6 grid grid-cols-custom items-center font-light gap-8">
+    <div className="h-[5.125rem] bg-white rounded-lg border-[1px] border-transparent hover:border-purple transition-colors duration-300 cursor-pointer py-4 px-6 grid grid-cols-custom items-center gap-8">
       <div>
         <p className="font-semibold text-text_dark">
           <span className="font-normal text-primary_text_color">#</span>
@@ -16,11 +17,12 @@ const InvoiceBar = ({ invoice }: InvoiceBarProps) => {
       <div>
         <p className="truncate">{invoice.client_name}</p>
       </div>
-      <div className="font-bold text-text_dark text-right text-[1.125rem]">
-        <p>£{invoice.total}</p>
+      <div className="font-bold text-text_dark text-right text-[1.25rem]">
+        <p>£{Number(invoice.total).toLocaleString()}</p>
       </div>
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center gap-4">
         <StatusTile status={invoice.status} />
+        <img src={rightArrow} alt="" />
       </div>
     </div>
   );
