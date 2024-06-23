e66d9453da47c953461233b0f2c8b2478c2fc294
Progress Aienobe <paienobe@gmail.com>
2024-06-23 21:43:06 +0100

    responsive_ui

--- client/invoice_app/src/components/Layout/Header/Header.tsx
+++ client/invoice_app/src/components/Layout/Header/Header.tsx
@@ -17,21 +17,21 @@ const Header = () => {
   }, [location.pathname]);
 
   return (
-    <header className="w-[5.625rem] bg-dark_blue h-screen fixed rounded-tr-[1.25rem] rounded-br-[1.25rem] overflow-hidden flex flex-col justify-between z-20">
+    <header className="w-[5.625rem] bg-dark_blue h-screen fixed rounded-tr-[1.25rem] rounded-br-[1.25rem] overflow-hidden flex flex-col md:flex-row justify-between z-20 md:top-0 md:h-[5.625rem] md:w-full md:rounded-none">
       <Link to="/">
-        <div className="w-full h-[5.625rem] flex items-center justify-center bg-purple rounded-br-[1.25rem] relative overflow-hidden group">
+        <div className="w-full md:w-[5.625rem] h-[5.625rem] md:h-full flex items-center justify-center bg-purple rounded-br-[1.25rem] md:rounded-tr-[1.25rem] relative overflow-hidden group">
           <img src={logo} className="z-10" alt="" />
           <div className="w-full h-1/2 group-hover:h-3/4 transition-all duration-500 absolute bg-white opacity-20 bottom-0 rounded-tl-[1.25rem]"></div>
         </div>
       </Link>
 
-      <div>
-        <div className="h-[4.5rem] flex items-center justify-center">
+      <div className="md:flex md:w-[12.5rem]">
+        <div className="h-[4.5rem] md:h-full md:w-1/2 flex items-center justify-center">
           <div className="cursor-pointer py-2 px-4 rounded-xl transition-colors duration-300 hover:bg-purple hover:bg-opacity-30">
             <img src={isDark ? moon : sun} alt="" />
           </div>
         </div>
-        <div className="h-[4.5rem] flex items-center justify-center border-t-[1.5px] border-white border-opacity-20">
+        <div className="h-[4.5rem] md:h-full flex items-center justify-center border-t-[1.5px] border-white border-opacity-20 md:w-1/2 md:border-t-transparent md:border-l-[1.5px]">
           <img
             className="w-[2.5rem] h-[2.5rem] rounded-full"
             src={user_image}
--- client/invoice_app/src/components/Layout/PageContainer/PageMainArea.tsx
+++ client/invoice_app/src/components/Layout/PageContainer/PageMainArea.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const PageMainArea = ({ children }: { children: ReactNode }) => {
   return (
-    <section className="w-[62.5%] max-w-[75rem] mx-auto py-12">
+    <section className="w-[62.5%] md:w-[90%] max-w-[75rem] mx-auto py-12 md:py-[calc(3rem+5.625rem)]">
       {children}
     </section>
   );
