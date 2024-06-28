f94dc88abf45feaba1243cafcdcee74af6aebc0f
Progress Aienobe <paienobe@gmail.com>
2024-06-28 20:01:47 +0100

    updated allowed hosts

--- backend/invoice_api/settings.py
+++ backend/invoice_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", default="")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['invoice-api-uaxk.onrender.com']
 
 
 # Application definition
